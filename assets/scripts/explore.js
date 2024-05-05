38f3ebc5f1ac087e6b61c9b174298c4275c4a35c
Sofia Nguyen <120623425+symsoph@users.noreply.github.com>
2024-05-05 08:28:06 -0700

    open and close mouth

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,31 +4,27 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   const synth = window.speechSynthesis;
-const button = document.querySelector("button");
-// const inputForm = document.querySelector("textarea");
-const inputTxt = document.getElementById("text-to-speak");
-const voiceSelect = document.querySelector("select");
-const faceImg = document.querySelector('img');
-// const pitch = document.querySelector("#pitch");
-// const pitchValue = document.querySelector(".pitch-value");
-// const rate = document.querySelector("#rate");
-// const rateValue = document.querySelector(".rate-value");
+  const button = document.querySelector("button");
+  const inputTxt = document.getElementById("text-to-speak");
+  const voiceSelect = document.querySelector("select");
+  const faceImg = document.querySelector('img');
 
-let voices = [];
 
-function populateVoiceList() {
-  voices = synth.getVoices().sort(function (a, b) {
-    const aname = a.name.toUpperCase();
-    const bname = b.name.toUpperCase();
+  let voices = [];
 
-    if (aname < bname) {
-      return -1;
-    } else if (aname == bname) {
-      return 0;
-    } else {
-      return +1;
-    }
-  });
+  function populateVoiceList() {
+    voices = synth.getVoices().sort(function (a, b) {
+      const aname = a.name.toUpperCase();
+      const bname = b.name.toUpperCase();
+
+      if (aname < bname) {
+        return -1;
+      } else if (aname == bname) {
+        return 0;
+      } else {
+        return +1;
+      }
+    });
   const selectedIndex =
     voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
   voiceSelect.innerHTML = "";
@@ -55,41 +51,34 @@ if (speechSynthesis.onvoiceschanged !== undefined) {
 }
 
 function speak() {
-  // if (synth.speaking) {
-  //   faceImg.src = 'assets/images/smiling-open.png';
-  // }
+  // when text is inputted
   if (inputTxt.value !== "") {
-    faceImg.src = 'assets/images/smiling-open.png';
     const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    faceImg.src = 'assets/images/smiling-open.png';
 
+    // when done speaking
     utterThis.onend = function (event) {
+      faceImg.src = 'assets/images/smiling.png';
       console.log("SpeechSynthesisUtterance.onend");
     };
-
+    
     utterThis.onerror = function (event) {
       console.error("SpeechSynthesisUtterance.onerror");
     };
-
+    
     const selectedOption =
-      voiceSelect.selectedOptions[0].getAttribute("data-name");
-
+    voiceSelect.selectedOptions[0].getAttribute("data-name");
+    
     for (let i = 0; i < voices.length; i++) {
       if (voices[i].name === selectedOption) {
         utterThis.voice = voices[i];
         break;
       }
-    }
-    synth.speak(utterThis);
-    // can not have source  twice in same function??
   }
-  if (!synth.speaking){
-    faceImg.src = 'assets/images/smiling.png';
-  }
-  
-  }
+  synth.speak(utterThis);
+}
+}
 
-button.addEventListener('click', (event) => {
-  speak();
-});
+button.addEventListener('click', speak);
 
 }

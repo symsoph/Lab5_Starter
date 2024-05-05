c1ced210f5349b7e4e45c93e0e316caa55fed455
Sofia Nguyen <120623425+symsoph@users.noreply.github.com>
2024-05-04 21:38:39 -0700

    sofia version

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Lab 5 - Starter
+Name: Sofia Nguyen
+
+Partner: Eugenie Ren
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,44 @@
 // expose.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener('DOMContentLoaded', init); // all content is loaded
 
 function init() {
-  // TODO
+  //button
+  let button = document.querySelector('button');
+  //audio file
+  let audio_file = new Audio(document.getElementsByClassName('hidden')[0]); // returns array, so need index
+  // alert(audio_file);
+  // console.log('audio_file.toString()');
+  //no other index works
+  const audio1 = new Audio("assets/audio/air-horn.mp3"); // works
+  const audio2 = new Audio("assets/audio/car-horn.mp3");
+  
+  let picture = document.querySelector('[alt = "No image selected"]');
+  // alert(picture);
+
+  // DO NOT USE EVER - picture.load();
+
+  const img = new Image(); // Create new img element
+  // img.src = "assets/images/car-horn.svg"; // Set source path
+
+  let type_horn = document.getElementById('horn-select');
+  // alert(type_horn);
+  // //true audio? 
+  //images
+
+  /**https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event */
+  type_horn.addEventListener("input", (event) => { // runs when user selects from dropdown
+    // audio2.play();
+    // picture.src = 'assets/images/${event.target.value}.svg'; // change source of picture
+    button.textContent = 'assets/images/ ${event.target.value} .svg';
+  });
+
+  button.addEventListener("click", (event) => {
+    // button.textContent = `Click count: ${event.detail}`;
+    // button.textContent = `Click count: ${audio1.toString()}`;
+    
+    audio1.play();
+    
+
+  });
 }
